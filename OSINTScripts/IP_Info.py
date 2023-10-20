d18929dbd353af016629bf7eec029a8773f5a7fc
Márton Reiter <marci.reiter501@gmail.com>
2023-10-20 14:36:41 +0200

    [Compatibility] Making small changes

--- OSINTScripts/IP_Info.py
+++ OSINTScripts/IP_Info.py
@@ -1,7 +1,7 @@
 class IP_Info:
     def __init__(self, read_ip: str) -> None:
         self.ip_address = read_ip
-        self.ip: str = ''
+        self.ip: str = self.ip_address.split('/')[0]
         self.network: str = ''
         self.virustotal: dict = {}
         self.ipqualityscore: dict = {}
