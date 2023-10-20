b17ae086e09abb8f56a15f5f0a2814baf9cb0be8
Márton Reiter <marci.reiter501@gmail.com>
2023-10-20 12:52:46 +0200

    [Compatibility] Making changes according to mypy

--- OSINTScripts/analyzing.py
+++ OSINTScripts/analyzing.py
@@ -3,7 +3,7 @@ import requests, sys, logging
 from IP_Info import IP_Info
 from argparse import ArgumentParser
 
-# This script will provide you with detailed information about each IP address, including its reputation, whois information, and the results from various antivirus engines.
+
 virustotal_api_key = 'REPLACE_API_KEY'
 ipqualityscore_api_key = 'REPLACE_API_KEY'
 
@@ -12,17 +12,17 @@ def configure_logging(working_directory: str) -> None:
     handlers: list[logging.Handler] = [logging.FileHandler(filename=log_path), logging.StreamHandler(stream=sys.stdout)]
     logging.basicConfig(format='%(asctime)s, %(levelname)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S', encoding='utf-8', level=logging.DEBUG, handlers=handlers)
 
-def parse_arguments() -> (str, str):
+def parse_arguments() -> tuple[str, str]:
     parser: ArgumentParser = ArgumentParser()
     parser.add_argument('-p', '--path', type=str, help='Path to file containing IPs.', required=True)
     parser.add_argument('-w', '--workdir', type=str, help='Working directory path.', required=True)
     args = parser.parse_args()
-    return (args.path, args.workdir)
+    return (str(args.path), str(args.workdir))
 
 def read_ips(filepath: str) -> list[IP_Info]:
     logging.info('Reading IPs from file: ' + filepath)
     with open(filepath, 'r') as ip_file:
-        return map(lambda read_ip: IP_Info(read_ip), map(lambda ip: ip.strip(), ip_file.readlines()))
+        return list(map(lambda read_ip: IP_Info(read_ip), map(lambda ip: ip.strip(), ip_file.readlines())))
 
 def virustotal(ips: list[IP_Info]) -> None:
     logging.info('Starting analyzing IPs with VirusTotal...')
@@ -34,21 +34,18 @@ def virustotal(ips: list[IP_Info]) -> None:
 
         if response.status_code == 200:
             data = response.json()
-
             data = data.get('data')
-            analyzed_ip: IP_Info = IP_Info()
 
-            # Basic IP information
-            analyzed_ip.ip = data.get('id')
-            analyzed_ip.network = data.get('attributes').get('network')
+            ip.ip = data.get('id')
+            ip.network = data.get('attributes').get('network')
 
-            analyzed_ip.virustotal['reputation'] = data.get('attributes').get('reputation')
+            ip.virustotal['reputation'] = data.get('attributes').get('reputation')
 
             last_analysis_stats = data.get('attributes').get('last_analysis_stats')
-            analyzed_ip.virustotal['harmless_count'] = last_analysis_stats.get('harmless')
-            analyzed_ip.virustotal['suspicious_count'] = last_analysis_stats.get('suspicious')
-            analyzed_ip.virustotal['malicious_count'] = last_analysis_stats.get('malicious')
-            analyzed_ip.virustotal['undetected_count'] = last_analysis_stats.get('undetected')
+            ip.virustotal['harmless_count'] = last_analysis_stats.get('harmless')
+            ip.virustotal['suspicious_count'] = last_analysis_stats.get('suspicious')
+            ip.virustotal['malicious_count'] = last_analysis_stats.get('malicious')
+            ip.virustotal['undetected_count'] = last_analysis_stats.get('undetected')
         else:
             logging.error(f'VirusTotal API response gave error for IP: {ip}, Status code: {response.status_code}')
 
@@ -83,7 +80,7 @@ def ipqualityscore(ips: list[IP_Info]) -> None:
 
 
 if __name__ == '__main__':
-    (ip_file_path, workdir): str = parse_arguments()
+    (ip_file_path, workdir) = parse_arguments()
     configure_logging(working_directory=workdir)
     ips: list[IP_Info] = read_ips(filepath=ip_file_path)
     virustotal(ips=ips)
