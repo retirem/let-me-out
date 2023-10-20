f0d1aeb71afcdd03041f5a96de8b3d37431699c5
Márton Reiter <marci.reiter501@gmail.com>
2023-10-20 11:32:27 +0200

    [Compatibility] Refactoring VirusTotal script to accept path as arguments to IPs, and workdir

--- OSINTScripts/VirusTotalAPI.py
+++ OSINTScripts/VirusTotalAPI.py
@@ -1,58 +1,72 @@
-import requests
-import json
+import requests, sys, logging
+
+from argparse import ArgumentParser
 
 # This script will provide you with detailed information about each IP address, including its reputation, whois information, and the results from various antivirus engines.
-api_key = 'XXXXXXXXX'
+api_key = 'REPLACE_API_KEY'
+
+class IP_Info:
+    def __init__(self) -> None:
+        self.ip: str = ''
+        self.network: str = ''
+        self.reputation: int = 0
+        self.harmless_count: int = 0
+        self.malicious_count: int = 0
+        self.suspicious_count: int = 0
+        self.undetected_count: int = 0
 
 
+def configure_logging(working_directory: str) -> None:
+    log_path: str = working_directory + '/virustotal.log'
+    handlers: list[logging.Handler] = [logging.FileHandler(filename=log_path), logging.StreamHandler(stream=sys.stdout)]
+    logging.basicConfig(format='%(asctime)s, %(levelname)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S', encoding='utf-8', level=logging.DEBUG, handlers=handlers)
 
-# List of IPs to check
-ips_to_check = ['91.240.189.0', '91.240.189.1']  # Add all IPs you want to analyze
+def parse_arguments() -> (str, str):
+    parser: ArgumentParser = ArgumentParser()
+    parser.add_argument('-p', '--path', type=str, help='Path to file containing IPs.', required=True)
+    parser.add_argument('-w', '--workdir', type=str, help='Working directory path.', required=True)
+    args = parser.parse_args()
+    return (args.path, args.workdir)
 
-for ip in ips_to_check:
-    url = f'https://www.virustotal.com/api/v3/ip_addresses/{ip}'
+def read_ips(filepath: str) -> list[str]:
+    logging.info("Reading IPs from file: " + filepath)
+    with open(filepath, 'r') as ip_file:
+        return map(lambda ip: ip.strip(), ip_file.readlines())
+
+def analyze_ips(ips: list[str]) -> list[IP_Info]:
+    logging.info('Starting analyzing IPs with VirusTotal...')
+    analyzed_ips: list[IP_Info] = []
+
     headers = {'x-apikey': api_key}
+    for ip in ips:
+        url = f'https://www.virustotal.com/api/v3/ip_addresses/{ip}'
+        response = requests.get(url, headers=headers)
 
-    response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
 
-    if response.status_code == 200:
-        data = response.json()
-        
-        # Basic IP information
-        ip_address = data["data"]["id"]
-        reputation = data["data"]["attributes"]["reputation"]
-        last_analysis_stats = data["data"]["attributes"]["last_analysis_stats"]
-        
-        print(f'IP Address: {ip_address}')
-        print(f'Reputation: {reputation}')
+            analyzed_ip: IP_Info = IP_Info()
 
-        # Last analysis statistics
-        print(f'Malicious: {last_analysis_stats["malicious"]}')
-        print(f'Suspicious: {last_analysis_stats["suspicious"]}')
-        print(f'Harmless: {last_analysis_stats["harmless"]}')
-        print(f'Undetected: {last_analysis_stats["undetected"]}')
+            # Basic IP information
+            analyzed_ip.ip = data['data']['id']
+            analyzed_ip.network = data['data']['attributes']['network']
+            analyzed_ip.reputation = data['data']['attributes']['reputation']
 
-        # Whois information
-        whois = data["data"]["attributes"]["whois"]
-        if whois:
-            try:
-                whois_dict = json.loads(whois)
-                print("Whois Information:")
-                for key, value in whois_dict.items():
-                    print(f'{key}: {value}')
-            except json.JSONDecodeError:
-                print(f"Error parsing Whois information: {whois}")
+            last_analysis_stats = data['data']['attributes']['last_analysis_stats']
+            analyzed_ip.harmless_count = last_analysis_stats['harmless']
+            analyzed_ip.suspicious_count = last_analysis_stats['suspicious']
+            analyzed_ip.malicious_count = last_analysis_stats['malicious']
+            analyzed_ip.undetected_count = last_analysis_stats['undetected']
+
+            analyzed_ips.append(analyzed_ip)
         else:
-            print("No Whois Information available for this IP")
+            logging.error(f'API response gave error for IP: {ip}, Status code: {response.status_code}')
 
-        # Scan results
-        scan_results = data["data"]["attributes"]["last_analysis_results"]
-        print("\nScan Results:")
-        for engine, result in scan_results.items():
-            print(f'Engine: {engine}')
-            print(f'Result: {result["result"]}')
-            print(f'Category: {result["category"]}\n')
+    logging.info('Finished analyzing IPs with VirusTotal.')
+    return analyzed_ips
 
-        print("-" * 40)
-    else:
-        print(f'IP: {ip}, Error: {response.status_code}')
+if __name__ == "__main__":
+    (ip_file_path, workdir): str = parse_arguments()
+    configure_logging(working_directory=workdir)
+    ips: list[str] = read_ips(filepath=ip_file_path)
+    analyzed_ips: list[IP_Info] = analyze_ips(ips=ips)
