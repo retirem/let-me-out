3e402b758c2de83bae80211c4a24d4ec5b21be7a
Márton Reiter <marci.reiter501@gmail.com>
2023-10-23 11:14:56 +0200

    [Database] Starting script to feed DB

--- database/import_ips.py
+++ database/import_ips.py
@@ -1,1 +1,24 @@
-# Run this script to import IPs from a given file into the DB
+import os, json
+
+from configparser import ConfigParser
+from ip_info import IP_Info
+
+
+def get_conf():
+    conf_path: str = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../script.conf')
+    config_parser: ConfigParser = ConfigParser()
+    config_parser.read(conf_path)
+    return config_parser.get('CONFIGS', 'workdir_todays')
+
+def read_ips():
+    analyzed_ips_path: str = os.path.join(working_directory, 'analyzed_ips.json')
+    with open(analyzed_ips_path, 'r') as ip_file:
+        analyzed_ips_json: dict = json.load(ip_file)
+        
+
+
+if __name__ == '__main__':
+    global working_directory
+    working_directory = get_conf()
+
+    ips: list[IP_Info] = read_ips()
