6543a5a06f947b71ed149b464ee195984b864d31
Márton Reiter <marci.reiter501@gmail.com>
2023-10-23 13:19:54 +0200

    [Database] Adding credentials to config and connection to db

--- database/import_ips.py
+++ database/import_ips.py
@@ -1,15 +1,17 @@
-import os, json
+import os, psycopg2
 
 from configparser import ConfigParser
 from OSINTScripts.IP_Info import IP_Info
 
 
-def get_conf() -> tuple[str, str]:
+def get_conf() -> tuple[str, str, str]:
     conf_path: str = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../script.conf')
     config_parser: ConfigParser = ConfigParser()
     config_parser.read(conf_path)
     return (config_parser.get('DBCredentials', 'user'),
-            config_parser.get('DBCredentials', 'password'))
+            config_parser.get('DBCredentials', 'password'),
+            config_parser.get('DBCredentials', 'database'))
 
 def ips_to_db(ips: list[IP_Info]):
-    (user, password) = get_conf()
+    (user, password, database) = get_conf()
+    db_connection = psycopg2.connect(f'dbname={database} user={user} password={password}')
