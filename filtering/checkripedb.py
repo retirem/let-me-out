ebd9f84c500462b62d7769e6eaa6a8dbb5f73236
GeryfromCPH <gsalom23@student.aau.dk>
2023-10-26 14:25:44 +0200

    [Filtering] RipeDB check script error handling.

--- filtering/checkripedb.py
+++ filtering/checkripedb.py
@@ -15,27 +15,37 @@ def get_conf() -> str:
 
 def check_ripe_database():
     input_filename: str = os.path.join(working_directory, 'blocked_ips_networks.txt')
-    with open(input_filename, "r") as file:
-        ips = file.read().splitlines()
+    try:
+        with open(input_filename, "r") as file:
+            ips = file.read().splitlines()
+        int_not_danish_byRIPEdb = 0
+        int_danish_byRIPEdb = 0
+        not_danish = set()
 
-    int_not_danish_byRIPEdb = 0
-    int_danish_byRIPEdb = 0
-    not_danish = set()
+        try:
+            for ip in ips:
+                url = f"http://rest.db.ripe.net/search?flags=no-personal&query-string={ip}"
+                respond = requests.get(url)
+                DK = '<attribute name="country" value="DK"/>'
+                dk = '<attribute name="country" value="dk"/>'
+                Dk = '<attribute name="country" value="Dk"/>'
+                dK = '<attribute name="country" value="dK"/>'
+                if (DK or dk or Dk or dK) in respond.text:
+                    int_danish_byRIPEdb += 1
+                else:
+                    int_not_danish_byRIPEdb += 1
+                    not_danish.add(ip)
+            with open(os.path.join(working_directory, 'ripeDB_check.txt'), "w") as output:
+                output.write(str(int_not_danish_byRIPEdb) + "\n")
+        except Exception as ex:
+            print("[-] Failed to send requests to the RIPE DB.")
+            print(ex)
+            sys.exit(1)
+    except Exception as ex:
+        print("[-] Failed to open the blocked_ips_networks.txt file.")
+        print(ex)
+        sys.exit(1)
 
-    for ip in ips:
-        url = f"http://rest.db.ripe.net/search?flags=no-personal&query-string={ip}"
-        respond = requests.get(url)
-        DK = '<attribute name="country" value="DK"/>'
-        dk = '<attribute name="country" value="dk"/>'
-        Dk = '<attribute name="country" value="Dk"/>'
-        dK = '<attribute name="country" value="dK"/>'
-        if (DK or dk or Dk or dK) in respond.text:
-            int_danish_byRIPEdb += 1
-        else:
-            int_not_danish_byRIPEdb += 1
-            not_danish.add(ip)
-    with open(os.path.join(working_directory, 'ripeDB_check.txt'), "w") as output:
-        output.write(str(int_not_danish_byRIPEdb) + "\n")
 
 
 if __name__ == "__main__":
