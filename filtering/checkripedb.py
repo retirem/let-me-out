1955c025b005a86ec8161d729595096664dc983a
GeryfromCPH <gsalom23@student.aau.dk>
2023-10-27 11:58:34 +0200

    [Checkripedb.py] The output to be two seperate txt for each IPs and Networks.

--- filtering/checkripedb.py
+++ filtering/checkripedb.py
@@ -10,16 +10,53 @@ def get_conf() -> str:
     config_parser.read(conf_path)
     return config_parser.get('CONFIGS', 'workdir_todays')
 
+def NETcheck_ripe_database():
+    input_net_filename: str = os.path.join(working_directory, 'blocked_networks.txt')
 
-def check_ripe_database():
-    input_filename: str = os.path.join(working_directory, 'blocked_ips_networks.txt')
     try:
-        with open(input_filename, "r") as file:
-            ips = file.read().splitlines()
-        int_not_danish_byRIPEdb = 0
-        int_danish_byRIPEdb = 0
-        not_danish = set()
+        with open(input_net_filename, "r") as file:
+            netw = file.read().splitlines()
+        net_not_danish_byRIPEdb = 0
+        net_danish_byRIPEdb = 0
+        net_not_danish = set()
+        try:
+            for nets in netw:
+                url = f"http://rest.db.ripe.net/search?flags=no-personal&query-string={nets}"
+                respond = requests.get(url)
+            DK = '<attribute name="country" value="DK"/>'
+            dk = '<attribute name="country" value="dk"/>'
+            Dk = '<attribute name="country" value="Dk"/>'
+            dK = '<attribute name="country" value="dK"/>'
+            if (DK or dk or Dk or dK) in respond.text:
+                net_danish_byRIPEdb += 1
+            else:
+                net_not_danish_byRIPEdb += 1
+                net_not_danish.add(ip)
+            with open(os.path.join(working_directory, 'NET_ripeDB_check.txt'), "w") as output:
+                output.write(str(net_not_danish_byRIPEdb) + "\n")
+        except Exception as ex:
+            print("[-] Failed to send requests (containing blocked networks) to the RIPE DB.")
+            print(ex)
+            sys.exit(1)
+    
+    except Exception as ex:
+        print("[-] Failed to open the blocked_ips_networks.txt file during the networks check.")
+        print(ex)
+        sys.exit(1)
 
+
+def IPcheck_ripe_database():
+    input_ip_filename: str = os.path.join(working_directory, 'unique_blocked_ips.txt')
+    
+    try:
+        with open(input_ip_filename, "r") as file:
+            ips = file.read().splitlines()
+    
+        ip_not_danish_byRIPEdb = 0
+        ip_danish_byRIPEdb = 0
+        ip_not_danish = set()
+
+        # Blocked ip check
         try:
             for ip in ips:
                 url = f"http://rest.db.ripe.net/search?flags=no-personal&query-string={ip}"
@@ -29,18 +66,18 @@ def check_ripe_database():
                 Dk = '<attribute name="country" value="Dk"/>'
                 dK = '<attribute name="country" value="dK"/>'
                 if (DK or dk or Dk or dK) in respond.text:
-                    int_danish_byRIPEdb += 1
+                    ip_danish_byRIPEdb += 1
                 else:
-                    int_not_danish_byRIPEdb += 1
-                    not_danish.add(ip)
-            with open(os.path.join(working_directory, 'ripeDB_check.txt'), "w") as output:
-                output.write(str(int_not_danish_byRIPEdb) + "\n")
+                    ip_not_danish_byRIPEdb += 1
+                    ip_not_danish.add(ip)
+            with open(os.path.join(working_directory, 'IP_ripeDB_check.txt'), "w") as output:
+                output.write(str(ip_not_danish_byRIPEdb) + "\n")
         except Exception as ex:
-            print("[-] Failed to send requests to the RIPE DB.")
+            print("[-] Failed to send requests (containing blocked IPs) to the RIPE DB.")
             print(ex)
             sys.exit(1)
     except Exception as ex:
-        print("[-] Failed to open the blocked_ips_networks.txt file.")
+        print("[-] Failed to open the blocked_ips_networks.txt file during the IPs check.")
         print(ex)
         sys.exit(1)
 
@@ -50,6 +87,7 @@ if __name__ == "__main__":
     global working_directory
     working_directory = get_conf()
 
-    check_ripe_database()
+    IPcheck_ripe_database()
+    NETcheck_ripe_database()
     
 
