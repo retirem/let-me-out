a633598f9e8371903a2f8af313ee4b1398de5da8
letmeoutbuildbot <letmeoutbuildbot@gmail.com>
2023-11-05 15:12:31 +0100

    [Baschscript] Finish the bash script.

--- mainBashScript.sh
+++ mainBashScript.sh
@@ -3,18 +3,23 @@
 set -e
 
 # Fetching all the blocked IPs from Firehol
-python3 fetch_ips/update_ips.py 
-
-echo "Fetching done" 
+python /home/letmeout/git/let-me-out/fetch_ips/update_ips.py
+echo "Fetching done"> /tmp/fetching.txt
 
 # Run compare_ips.py to filter the fetched IPs and save to the output file
-python3 filtering/compare_ips.py --blocklist "../aggregated_iplists.txt" --networks "../dk-aggregated.zone.txt" 
-
-echo "Filtering done" 
+python /home/letmeout/git/let-me-out/filtering/compare_ips.py
+echo "Filtering done"> /tmp/filtering.txt
 
 # Run analyzing.py to see which IP adresses are malicious and their quality score
-python3 OSINTScripts/analyzing.py -w "../"
+python /home/letmeout/git/let-me-out/OSINTScripts/analyzing.py
+echo "Maliciousness and quality tested."> /tmp/analyzing.txt
 
-echo "Maliciousness and quality tested. Results are in:"
+#Run the ripecheck script
+#python /home/letmeout/git/let-me-out/filtering/checkripedb.py
 
-# Remember to give permission to bash script with; chmod -x mainBashScript.sh 
+# Upload data to the database
+python /home/letmeout/git/let-me-out/database/txt_to_db.py
+echo "Data upload to the database is done."> /tmp/database.txt
+
+
+
